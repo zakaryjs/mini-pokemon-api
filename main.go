9f9b77c016b61a32ad0d27a4d1d5cf9e77fa882f
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-04-20 16:08:51 +0800

    feat: added route to get all pokemon

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Pokemon struct {
+	Species  string `json:"species"`
+	Nickname string `json:"nickname"`
+	Level    int    `json:"level"`
+}
+
+var PokemonStruct = []Pokemon{
+	{Species: "Samurott", Nickname: "ASH", Level: 100},
+	{Species: "Xerneas", Nickname: "Xerneas", Level: 100},
+	{Species: "Pikachu", Nickname: "Bolt", Level: 25},
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/pokemon", getPokemon)
+
+	router.Run("localhost:8080")
+}
+
+func getPokemon(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, PokemonStruct)
+}
