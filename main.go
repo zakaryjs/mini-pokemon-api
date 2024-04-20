df37cfc8a8f24cf670ebaa3f994b98de4261ad14
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-04-20 16:17:15 +0800

    feat: added get pokemon by id GET route

--- main.go
+++ main.go
@@ -7,25 +7,28 @@ import (
 )
 
 type Pokemon struct {
+	ID       string `json:"id"`
 	Species  string `json:"species"`
 	Nickname string `json:"nickname"`
 	Level    int    `json:"level"`
 }
 
 var PokemonStruct = []Pokemon{
-	{Species: "Samurott", Nickname: "ASH", Level: 100},
-	{Species: "Xerneas", Nickname: "Xerneas", Level: 100},
-	{Species: "Pikachu", Nickname: "Bolt", Level: 25},
+	{ID: "1", Species: "Samurott", Nickname: "ASH", Level: 100},
+	{ID: "2", Species: "Xerneas", Nickname: "Xerneas", Level: 100},
+	{ID: "3", Species: "Pikachu", Nickname: "Bolt", Level: 25},
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/pokemon", getPokemon)
+	router.GET("/pokemon", GetPokemon)
+	router.GET("/pokemon/:id", GetPokemonByID)
+	router.POST("/pokemon", CreatePokemon)
 
 	router.Run("localhost:8080")
 }
 
-func getPokemon(c *gin.Context) {
+func GetPokemon(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, PokemonStruct)
 }
 
@@ -38,4 +41,17 @@ func CreatePokemon(c *gin.Context) {
 
 	PokemonStruct = append(PokemonStruct, newPokemon)
 	c.IndentedJSON(http.StatusCreated, newPokemon)
+}
+
+func GetPokemonByID(c *gin.Context) {
+
+	id := c.Param("id")
+
+	for _, a := range PokemonStruct {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "There was no Pokemon found with that ID."})
 }
